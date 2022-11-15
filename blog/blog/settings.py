b61cf51f022e7b70035ee1c56d1e2d47456c521c
Mahbub-E-Sobhani Himel <himel448@gmail.com>
2022-11-15 16:58:29 +0600

    SECURITY_KEY erased

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -26,7 +26,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zc0^@6b&***6q^9-^jvxdaze5f_!t=agjj!)jwh8-$h7d6jh9-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
